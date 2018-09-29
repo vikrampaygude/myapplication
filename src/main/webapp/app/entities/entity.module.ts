168dd82b274a8c8bd70bd5f3dcc85745af13e24a
vikram <vikrampaygude@gmail.com>
2018-09-29 13:23:08 +0530

    new code

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,26 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { MyapplicationRegionModule } from './region/region.module';
+import { MyapplicationCountryModule } from './country/country.module';
+import { MyapplicationLocationModule } from './location/location.module';
+import { MyapplicationDepartmentModule } from './department/department.module';
+import { MyapplicationTaskModule } from './task/task.module';
+import { MyapplicationEmployeeModule } from './employee/employee.module';
+import { MyapplicationJobModule } from './job/job.module';
+import { MyapplicationJobHistoryModule } from './job-history/job-history.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        MyapplicationRegionModule,
+        MyapplicationCountryModule,
+        MyapplicationLocationModule,
+        MyapplicationDepartmentModule,
+        MyapplicationTaskModule,
+        MyapplicationEmployeeModule,
+        MyapplicationJobModule,
+        MyapplicationJobHistoryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
---
+++ src/main/webapp/app/entities/location/location.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MyapplicationSharedModule } from 'app/shared';
+import {
+    LocationComponent,
+    LocationDetailComponent,
+    LocationUpdateComponent,
+    LocationDeletePopupComponent,
+    LocationDeleteDialogComponent,
+    locationRoute,
+    locationPopupRoute
+} from './';
+
+const ENTITY_STATES = [...locationRoute, ...locationPopupRoute];
+
+@NgModule({
+    imports: [MyapplicationSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    declarations: [
+        LocationComponent,
+        LocationDetailComponent,
+        LocationUpdateComponent,
+        LocationDeleteDialogComponent,
+        LocationDeletePopupComponent
+    ],
+    entryComponents: [LocationComponent, LocationUpdateComponent, LocationDeleteDialogComponent, LocationDeletePopupComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MyapplicationLocationModule {}
